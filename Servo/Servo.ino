66751eea9fb76cd214736572881cbe5173d99943
Preet Pancholi <preetpancholi22@gmail.com>
2016-12-06 16:11:42 -0600

    Add files via upload

---
+++ Servo/Servo.ino
@@ -0,0 +1,48 @@
+#include <usbhid.h>
+#include <hiduniversal.h>
+#include <usbhub.h>
+
+// Satisfy IDE, which only needs to see the include statment in the ino.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#include <SPI.h>
+#endif
+
+
+
+#include "hidjoystickrptparser.h"
+
+USB Usb;
+USBHub Hub(&Usb);
+HIDUniversal Hid(&Usb);
+JoystickEvents JoyEvents;
+chutya chutya1;
+JoystickReportParser Joy(&JoyEvents);
+
+void setup() {
+  pinMode(chutya1.in1Pin, OUTPUT);
+  pinMode(chutya1.in2Pin, OUTPUT);
+  pinMode(chutya1.enablePin1, OUTPUT);
+  pinMode(chutya1.in3Pin, OUTPUT);
+  pinMode(chutya1.in4Pin, OUTPUT);
+  pinMode(chutya1.enablePin2, OUTPUT);
+   JoyEvents.chutya2.myservo.attach(chutya1.servoPin);
+        Serial.begin(115200);
+#if !defined(__MIPSEL__)
+        while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+        Serial.println("Start");
+
+        if (Usb.Init() == -1)
+                Serial.println("OSC did not start.");
+
+        delay(200);
+
+        if (!Hid.SetReportParser(0, &Joy))
+                ErrorMessage<uint8_t > (PSTR("SetReportParser"), 1);
+}
+
+void loop() {
+        Usb.Task();
+}
+
---
+++ Servo/hidjoystickrptparser.h
@@ -0,0 +1,47 @@
+#if !defined(__HIDJOYSTICKRPTPARSER_H__)
+#define __HIDJOYSTICKRPTPARSER_H__
+
+#include <usbhid.h>
+#include <Servo.h>
+
+class chutya
+{
+  public:
+int enablePin1 = 4;
+int in1Pin = 5;
+int in2Pin = 0;
+int in3Pin = 2;
+int in4Pin = 3;
+int enablePin2 = 1;
+Servo myservo;
+int servoPin = 6;
+};
+
+struct GamePadEventData {
+        uint8_t X, Y, Z1, Z2, Rz;
+};
+
+class JoystickEvents {
+public:
+  chutya chutya2;
+        virtual void OnGamePadChanged(const GamePadEventData *evt);
+        virtual void OnHatSwitch(uint8_t hat);
+        virtual void OnButtonUp(uint8_t but_id);
+        virtual void OnButtonDn(uint8_t but_id);
+};
+
+#define RPT_GEMEPAD_LEN		5
+
+class JoystickReportParser : public HIDReportParser {
+        JoystickEvents *joyEvents;
+
+        uint8_t oldPad[RPT_GEMEPAD_LEN];
+        uint8_t oldHat;
+        uint16_t oldButtons;
+public:
+        JoystickReportParser(JoystickEvents *evt);
+
+        virtual void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+};
+
+#endif // __HIDJOYSTICKRPTPARSER_H__
